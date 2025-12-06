cc1c33c9ca77256d63a9ae585c0614d34c470d89
DavidfariaGIT <davidfaria17@gmail.com>
2025-12-05 23:34:02 -0500

    changed the tasks to use objects and added localStorage

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
+const loadStorage = JSON.parse(localStorage.getItem('taskLocal')) || 'empty';
+console.log(loadStorage);
+
+
 
 const form = document.getElementById('form');
 const taskInput = document.getElementById('task-input');
 const tasksDisplay = document.getElementById('tasks-display');
+const deleteCon = document.getElementById('delete-container');
+const datePosted = document.getElementById('date-posted');
+const check = document.getElementById('check');
+
+
 const today = new Date();
 
 let dd = String(today.getDate()).padStart(2, '0');
@@ -11,62 +20,85 @@ let yy = String(today.getFullYear());
 let timePosted = mm + "/" + dd + "/" + yy;
 
 
+//drop delete
+deleteCon.addEventListener('dragover', (e) => {
+    e.preventDefault();
+});
+
+
+deleteCon.addEventListener('drop', (e) => {
+    e.preventDefault();
+    const draggedId = e.dataTransfer.getData('text/plain');
+    let removeId = document.getElementById(draggedId);
+    removeId.remove();
+});
+
+
+//set tasks
 const tasklist = [];
+const taskObject = {};
+
+function displayLocal() {
+if (loadStorage !== "empty") {
+    loadStorage.forEach(e => {
+        const randNum = Math.floor((Math.random() * 100) + 1);
+
+        const taskEl = document.createElement('div');
+        taskEl.classList.add("task");
+        taskEl.id = `${randNum}`
+        taskEl.draggable = "true";
+        taskEl.innerHTML = `
+    <p id="date-posted">${e.date}</p>
+    <p id="task-name">${e.taskname}</p>
+    <input id="check" name="isChecked" type="checkbox">
+    <button id="edit-button">Edit task</button>
+    `
+        tasksDisplay.appendChild(taskEl);
+    });
+}};
+
+displayLocal();
 
 form.addEventListener('submit', (e) => {
-
     e.preventDefault();
+    taskObject.date = timePosted;
+
     const userinputTask = taskInput.value;
-    const newDiv = document.createElement('div');
-    newDiv.textContent = userinputTask;
-    newDiv.id = 'task';
-    newDiv.draggable = "true"
+    taskObject.taskname = userinputTask;
 
-    newDiv.innerHTML =
-        `
-<p id="date-posted">${timePosted}</p>
-<p id="task-name">${userinputTask}</p>
-<input id="check" name="isChecked" type="checkbox">
-<button id="edit-button">Edit task</button>
-`
+    tasklist.push(taskObject);
+        localStorage.setItem('taskLocal', JSON.stringify(tasklist));
 
-    tasklist.push(newDiv);
+    const randNum = Math.floor((Math.random() * 100) + 1);
 
-    tasklist.forEach((x) => {
-        tasksDisplay.prepend(x);
-    });
+    const taskEl = document.createElement('div');
+    taskEl.classList.add("task");
+    taskEl.id = `${randNum}`
+    taskEl.draggable = "true";
+    taskEl.innerHTML = `
+    <p id="date-posted">${taskObject.date}</p>
+    <p id="task-name">${taskObject.taskname}</p>
+    <input id="check" name="isChecked" type="checkbox">
+    <button id="edit-button">Edit task</button>
+    `
+    tasksDisplay.appendChild(taskEl);
+   localStorage.setItem('taskLocal', JSON.stringify(tasklist));
+    const newTask = taskEl;
 
     const editButton = document.getElementById('edit-button');
     const taskName = document.getElementById('task-name');
+
+    //edit text
     editButton.addEventListener('click', () => {
         taskName.contentEditable = "true";
         taskName.focus();
-        editButton.disabled = "true";
     });
 
     //dragable 
-    const deleteCon = document.getElementById('delete-container');
-
-    newDiv.addEventListener('dragstart', (e) => {
+    newTask.addEventListener('dragstart', (e) => {
         e.dataTransfer.setData('text/plain', e.target.id);
         e.dataTransfer.effectAllowed = "move";
     })
-
-    deleteCon.addEventListener('dragover', (e) => {
-        e.preventDefault();
-    });
-
-    
-    deleteCon.addEventListener('drop', (e) => {
-        e.preventDefault
-        const draggedId = e.dataTransfer.getData('text/plain');
-        let removeId = document.getElementById(draggedId);
-        removeId.remove();
-
-     
-    });
-
-
 
 });
 
