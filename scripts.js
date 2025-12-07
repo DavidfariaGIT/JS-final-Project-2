6a84772140228684d6b595dcf84343f4b8e7b06b
DavidfariaGIT <davidfaria17@gmail.com>
2025-12-06 22:02:51 -0500

    recreated the renderTasks, and clearTasks inside function for re-usegble and cleaner code

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-const loadStorage = JSON.parse(localStorage.getItem('taskLocal')) || 'empty';
-console.log(loadStorage);
+const loadStorage = JSON.parse(localStorage.getItem("taskLocal")) || "empty";
 
 
 
@@ -19,12 +18,28 @@ let yy = String(today.getFullYear());
 
 let timePosted = mm + "/" + dd + "/" + yy;
 
+function makeRandId() {
+    const randNum = Math.floor(Math.random() * 100) + 1;
+    return randNum;
+}
 
-//drop delete
-deleteCon.addEventListener('dragover', (e) => {
+tasklist = [];
+
+//creating task obejcts 
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-});
 
+    const taskObject = {
+        id: makeRandId(),
+        date: timePosted,
+        name: taskInput.value,
+    };
+
+    tasklist.push(taskObject);
+    localStorage.setItem('taskLocal', JSON.stringify(tasklist));
+
+    renderTask(taskObject);
+});
 
 deleteCon.addEventListener('drop', (e) => {
     e.preventDefault();
@@ -34,77 +49,57 @@ deleteCon.addEventListener('drop', (e) => {
 });
 
 
-//set tasks
-const tasklist = [];
-const taskObject = {};
 
-function displayLocal() {
-if (loadStorage !== "empty") {
-    loadStorage.forEach(e => {
-        const randNum = Math.floor((Math.random() * 100) + 1);
+function renderTask(task) {
+    let newTask = document.createElement('div');
 
-        const taskEl = document.createElement('div');
-        taskEl.classList.add("task");
-        taskEl.id = `${randNum}`
-        taskEl.draggable = "true";
-        taskEl.innerHTML = `
-    <p id="date-posted">${e.date}</p>
-    <p id="task-name">${e.taskname}</p>
-    <input id="check" name="isChecked" type="checkbox">
-    <button id="edit-button">Edit task</button>
+    newTask.id = task.id;
+    newTask.classList.add('task');
+    newTask.draggable = "true;"
+    newTask.innerHTML =
+        `
+    <p id="date-posted">${task.date}</p>
+    <p class="task-name">${task.name}</p>
+    <input class="check" type="checkbox">
+    <button class="edit-button">Edit task</button>
     `
-        tasksDisplay.appendChild(taskEl);
+    tasksDisplay.appendChild(newTask);
+
+    newTask.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData("text/plain", e.target.id);
+        e.dataTransfer.effectAllowed = "move";
     });
-}};
+};
 
-displayLocal();
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    taskObject.date = timePosted;
+if (loadStorage !== "empty") {
+    loadStorage.forEach(task => {
+        renderTask(task);
+    })
+}
 
-    const userinputTask = taskInput.value;
-    taskObject.taskname = userinputTask;
 
-    tasklist.push(taskObject);
-        localStorage.setItem('taskLocal', JSON.stringify(tasklist));
-
-    const randNum = Math.floor((Math.random() * 100) + 1);
-
-    const taskEl = document.createElement('div');
-    taskEl.classList.add("task");
-    taskEl.id = `${randNum}`
-    taskEl.draggable = "true";
-    taskEl.innerHTML = `
-    <p id="date-posted">${taskObject.date}</p>
-    <p id="task-name">${taskObject.taskname}</p>
-    <input id="check" name="isChecked" type="checkbox">
-    <button id="edit-button">Edit task</button>
-    `
-    tasksDisplay.appendChild(taskEl);
-   localStorage.setItem('taskLocal', JSON.stringify(tasklist));
-    const newTask = taskEl;
-
-    const editButton = document.getElementById('edit-button');
-    const taskName = document.getElementById('task-name');
-
-    //edit text
-    editButton.addEventListener('click', () => {
+tasksDisplay.addEventListener('click', (e) => {
+    if (e.target.classList.contains("edit-button")) {
+        const parent = e.target.closest(".task");
+        const taskName = parent.querySelector(".task-name");
         taskName.contentEditable = "true";
         taskName.focus();
+    }
+
+    deleteCon.addEventListener('dragover', (e) => {
+        e.preventDefault();
     });
-
-    //dragable 
-    newTask.addEventListener('dragstart', (e) => {
-        e.dataTransfer.setData('text/plain', e.target.id);
-        e.dataTransfer.effectAllowed = "move";
-    })
-
 });
 
+function clearTasks() {
+    const clearButton = document.getElementById('clear-button');
+    clearButton.addEventListener("click", () => {
+    tasksDisplay.innerHTML = "";
+    localStorage.clear();
+    
 
+    })
+};
 
-
-
-
-
+clearTasks();
